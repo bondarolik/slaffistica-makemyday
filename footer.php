e82ef0ce9a5f18e5032e8d5091afb6be5a7dc1f2
Slaff <slaff.b@lipresso.com>
2012-08-29 19:54:03 -0400

    Code cleanup

--- footer.php
+++ footer.php
@@ -1,5 +1,5 @@
 <footer>
-	<p>&copy; 2004-2010, Вячеслав Бондарук. Прежде, чем что-то скопипастить, <a href="http://www.slaff.net/blog/feedback">уведомите</a> автора.</p>
+	<p>&copy; 2004-2010, <? bloginfo('name'); ?></p>
 	
 	<ul>
 		<li><a href="<?php bloginfo('rss2_url'); ?>" id="rss">Записи</a> (RSS)</li>
@@ -11,30 +11,6 @@
 </footer>
 </div> <!-- #slaffistica -->
 <?php wp_footer(); ?>
-<? /* 
-<script type='text/javascript' src='http://cdn.wibiya.com/Toolbars/dir_0601/Toolbar_601791/Loader_601791.js?ver=3.0.1'></script>
-*/ ?>
-<!-- Yandex.Metrika -->
-<script src="//mc.yandex.ru/metrika/watch.js" type="text/javascript"></script>
-<div style="display:none;">
-<script type="text/javascript">
-try { var yaCounter234492 = new Ya.Metrika(234492); } catch(e){}
-</script>
 </div>
-<noscript>
-	<div style="position:absolute"><img src="//mc.yandex.ru/watch/234492" alt="" /></div>
-</noscript>
-<!-- /Yandex.Metrika -->
-<script type="text/javascript">
-  var _gaq = _gaq || [];
-  _gaq.push(['_setAccount', 'UA-102994-4']);
-  _gaq.push(['_trackPageview']);
-
-  (function() {
-    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-  })();
-</script>
 </body>
 </html>
--- sidebar.php
+++ sidebar.php
@@ -5,15 +5,8 @@
 		<li id="home"><a href="http://www.slaff.net/blog/">Главная</a></li>
 		<li id="about"><a href="http://www.slaff.net/blog/colophon/ru">Об авторе</a></li>
 		<li id="arhivces"><a href="http://www.slaff.net/blog/archives">Архивы блога</a></li>
-		<li id="services"><a href="http://www.slaff.net/blog/uslugi">Услуги</a></li>
-		<li id="inmigration"><a href="http://www.slaff.net/blog/immigratsiya-v-chili">Иммиграция в Чили</a></li>
-		<? /* <li id="photostore"><a href="http://www.slaff.net/blog/sale">Фотомагазин</a></li> */ ?>
 		<li id="blogkey"><a href="http://www.slaff.net/blog/blog-pod-klyuch">Блог под ключ</a></li>
 		<li id="feedback"><a href="http://www.slaff.net/blog/feedback">Обратная связь</a></li>
-		<? /* 
-		<li><a href="http://www.slaff.net/blog/vash-turisticheskiy-konsultant">Туристические консультации</a></li>
-		<li><a href="http://www.slaff.net/blog/?p=2869" title="Весенний фотозамут"><span style="color: red !important; font-weight: bold;">Весенний фотозамут</span></a></li>
-		*/ ?>		
 	</ul>
 
 	<h2>Оглавление блога</h2>
@@ -52,55 +45,9 @@
 	<?php endif; # post 2660,2661,2664,2665,2666,2691 ?>
 </nav>
 
-	<? /* 
-	<h2>Со мной на связи</h2>
-	<ul>	
-		<li><a href="http://www.twitter.com/slaff" title="Slaff в Твиттере">Я в твиттере</a></li>
-		<li><a href="http://fusion.google.com/add?feedurl=http://feeds.feedburner.com/slaff">Читать в Google Reader</a></li>
-		<li><a href="http://lenta.yandex.ru/settings.xml?name=feed&amp;url=http://feeds.feedburner.com/slaff">Читать в Яндекс.Ленте</a></li>
-		<li><a href="http://feeds.feedburner.com/slaff"><img src="http://feeds.feedburner.com/~fc/slaff?bg=FFFFFF&amp;fg=36393D&amp;anim=1" height="26" width="88" alt="Подписаться на обновления блога по RSS" /></a></li>
-
-		<li><a href="<?php bloginfo('rss2_url'); ?>">Записи</a> (RSS)</li>
-		<li><a href="<?php bloginfo('comments_rss2_url'); ?>">Комментарии</a> (RSS)</li>
-	</ul>	
-		*/ ?>
 <?php /* If this is the frontpage */
   if ( (is_home()) && !(is_page()) && !(is_single()) && !(is_search()) && !(is_archive()) && !(is_author()) && !(is_category()) && !(is_paged()) ): ?>
 <?php endif; #frontpage ?>
 
-	<aside class="sponsors">
-		<h2>Спонсорские ссылки</h2>
-		<p id="tnx">
-		<?php
-		$cache_dir = '/_tnxcchedir/'; // you MUST insert your folder name instead of "cache"!
-		include_once($_SERVER['DOCUMENT_ROOT'] . $cache_dir . 'tnx.php');
-		$tnx = new TNX_n('slaff', $cache_dir); // your TNX login
-		echo $tnx->show_link(1); // first link
-		echo $tnx->show_link(1); // second link
-		echo $tnx->show_link(1); // third link
-		echo $tnx->show_link(); // remaining links
-		?>
-		</p>
-
-		<p id="sape">
-		<?php
-		  if (!defined('_SAPE_USER')) {
-		      define('_SAPE_USER', '9aaa6adeea1c7f76a22998aef2db6935');
-		  }
-		  require_once($_SERVER['DOCUMENT_ROOT'].'/'._SAPE_USER.'/sape.php');
-		  $o['charset'] = 'UTF-8';
-		  $sape = new SAPE_client($o);
-		  #echo $sape->return_links();
-			echo $sape->return_block_links();
-		  unset($o);
-		?>
-		</p>
-		
-		<? /* 
-		<p id="setlinks" >
-		<?php require_once($_SERVER['DOCUMENT_ROOT']."/blog/setlinks_3f04b/slsimple.php"); ?>
-		</p>
-		*/ ?>
-	</aside>
 </section> <!-- section.sidebar -->	
 <!-- sidebar ends -->
